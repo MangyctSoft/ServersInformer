5d449b096d06c4c87ab7ff6a4ea6bfed90966a22
Mangyct <andrei-df@mail.ru>
2020-03-02 19:09:37 +0300

    Add project files.

---
+++ ServersInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServersInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
