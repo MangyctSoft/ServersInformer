625ac432ac6bf01c2f7b9b44b2e869ad2cc447d0
Mangyct <andrei-df@mail.ru>
2020-03-04 12:17:14 +0300

    Структура конфигурационного файла

--- ServersInfo/Program.cs
+++ ServersInfo/Program.cs
@@ -9,6 +9,20 @@ using ServersInfo.Models;
 
 namespace ServersInfo
 {
+    /*
+     * Конфигурационный файл создается автоматически при первом запуске приложения,
+     * в корневой папке программы.
+     * 
+     * Далее необходимо скачать настройки для доступа к Google Sheets (client_secret.json),
+     * закинуть в корень программы и прописать его в конфигурационном файле в секции <ClientJson>.
+     * 
+     * Открыть Google Sheets https://docs.google.com/spreadsheets/d/ [1tOC2Hz2kZ-######-co5JIxA9-#########] /edit#gid=0
+     * и скопировать из адресной строки id документа формата - 1tOC2Hz2kZ-######-co5JIxA9-#########,
+     * прописать этот id в конфигурационном файле в секции <SpreadsheetId>.
+     * 
+     * В секцию <Servers> добавить данный о серверах в формате 
+     * <Server Host="_hostname_" Username="_username_" Password="_password_" Port="_5432_" Size="_размер диска_" />
+     */
     class Program
     {
         static IDatabaseHelper dbHelper;            // Хелпер для работы с базами данных
